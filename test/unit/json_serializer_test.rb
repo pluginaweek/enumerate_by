b023a264bbadcf20a58ef6e53af3623c15aee634
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2010-02-27 13:42:12 -0500

    Fix tests failing on Rails 2.3.3+

--- test/unit/json_serializer_test.rb
+++ test/unit/json_serializer_test.rb
@@ -8,11 +8,11 @@ class JSONSerializerTest < ActiveRecord::TestCase
   end
   
   def test_should_include_enumeration_in_json
-    assert_match %r{"color": "red"}, @json
+    assert_match %r{"color":\s*"red"}, @json
   end
   
   def test_should_render_other_attributes
-    assert_match %r{"id": #{@car.id}}, @json
-    assert_match %r{"name": "Ford Mustang"}, @json
+    assert_match %r{"id":\s*#{@car.id}}, @json
+    assert_match %r{"name":\s*"Ford Mustang"}, @json
   end
 end
